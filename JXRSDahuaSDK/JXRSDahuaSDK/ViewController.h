892dcd7957a368a7bdf20f43c30506a0f54d2f0b
tao cheng <>
2018-11-02 10:36:23 +0800

    add sdk

---
+++ JXRSDahuaSDK/JXRSDahuaSDK/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JXRSDahuaSDK
+//
+//  Created by tao cheng on 2018/11/2.
+//  Copyright © 2018 tao cheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ JXRSDahuaSDK/JXRSDahuaSDK/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  JXRSDahuaSDK
+//
+//  Created by tao cheng on 2018/11/2.
+//  Copyright © 2018 tao cheng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
